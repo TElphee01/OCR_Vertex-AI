de06de342c3546fe5ddafaa5bf0e4fffbeb727eb
telph <telpheemail@aol.com>
2023-05-05 17:24:38 -0400

    Cleaning up project

--- main.py
+++ main.py
@@ -14,21 +14,42 @@
 
 # [START cloudrun_imageproc_controller]
 # [START run_imageproc_controller]
+import os
+from torch import multiprocessing
 import base64
 import json
-import os
+import tempfile
 
-from flask import Flask, request
+from chat_ocr import decode_image
 
-import image
+from flask import Flask, request, Response
+from google.cloud import storage, firestore
+from google.oauth2 import service_account
+# Specify the path to the JSON key file in the container
+key_path = "credentials.json"
+
+# Create credentials from the JSON key file
+credentials = service_account.Credentials.from_service_account_file(key_path)
+
+# Instantiate a Google Cloud Storage client using the credentials
+storage_client = storage.Client(credentials=credentials)
 
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST"])
+@app.route("/isalive")
+def is_alive():
+    print("/isalive request")
+    status_code = Response(status=200)
+    return status_code
+
+
+@app.route("/predict", methods=["POST"])
 def index():
     envelope = request.get_json()
+    from pprint import pprint
+    pprint(envelope)
     if not envelope:
         msg = "no Pub/Sub message received"
         print(f"error: {msg}")
@@ -64,7 +85,35 @@ def index():
             return f"Bad Request: {msg}", 400
 
         try:
-            image.process_images(data)
+            # Fetch image location from cloud storage
+            file_data = data
+            file_name = file_data["name"]
+            bucket_name = file_data["bucket"]
+
+            # blob = storage_client.bucket(bucket_name).get_blob(file_name)
+            # blob_uri = f"gs://{bucket_name}/{file_name}"
+
+            bucket = storage_client.get_bucket(bucket_name)
+            blob = bucket.blob(file_name)
+
+            print("Start of main")
+            print(data)
+            print(bucket_name)
+
+            print(bucket)
+            print(blob)
+            print(file_name)
+            _, temp_local_filename = tempfile.mkstemp()
+
+            print(temp_local_filename)
+
+            blob.download_to_filename(temp_local_filename)
+            print(type(temp_local_filename))
+            print("TEST HERE2222")
+
+            print(type(blob))
+            print(temp_local_filename)
+            decode_image(blob)
             return ("", 204)
 
         except Exception as e:
@@ -76,9 +125,13 @@ def index():
     # [END cloudrun_imageproc_controller]
 
 
+print(__name__)
+
 if __name__ == "__main__":
+    print("IN main")
+    multiprocessing.set_start_method("spawn", force=True)
     PORT = int(os.getenv("PORT")) if os.getenv("PORT") else 8080
 
     # This is used when running locally. Gunicorn is used to run the
     # application on Cloud Run. See entrypoint in Dockerfile.
-    app.run(host="127.0.0.1", port=PORT, debug=True)
+    app.run(host="127.0.0.1", port=PORT, debug=True, threaded=False)
